9dfb0a7af4954b0636163da182366e45a963eb0b
Bobby De Luna <bobby.deluna2@gmail.com>
2023-02-14 23:11:09 -0700

    commit tree 06a8f97cdc91852f815e909f7c3a3e4485d56405

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :items, only: [:index]
+  
+  resources :items, only: [:index] do
+    resources :users, only: [:show, :index]
+  end
+
   resources :users, only: [:show]
 end
